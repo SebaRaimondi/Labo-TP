779be81a373149acf460099c2b1990ddfc861365
Sebastian Raimondi <sebastianraimondi@gmail.com>
2018-12-04 03:04:41 -0300

    Updated Layouts

--- app/src/main/java/raimondirios/razasypelajes/ConfigActivity.java
+++ app/src/main/java/raimondirios/razasypelajes/ConfigActivity.java
@@ -3,11 +3,10 @@ package raimondirios.razasypelajes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class RazasPelajes extends AppCompatActivity {
+public class ConfigActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.interacion_b);
     }
 }
